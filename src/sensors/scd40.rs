1c68c9b49629d1fb9fbcdca55924830e179f0c87
Sylvain Bouchard <sylvain.bouchard@live.ca>
2026-07-10 18:14:16 -0400

    refactor: fix the SCD40 start sequence

--- src/sensors/scd40.rs
+++ src/sensors/scd40.rs
@@ -11,6 +11,9 @@ const SCD40_ADDR: u8 = 0x62;
 const CMD_START_PERIODIC_MEASUREMENT: [u8; 2] = [0x21, 0xB1];
 const CMD_GET_DATA_READY_STATUS: [u8; 2] = [0xE4, 0xB8];
 const CMD_READ_MEASUREMENT: [u8; 2] = [0xEC, 0x05];
+const CMD_WAKE_UP: [u8; 2] = [0x36, 0xF6];
+const CMD_STOP_PERIODIC_MEASUREMENT: [u8; 2] = [0x3F, 0x86];
+const CMD_REINIT: [u8; 2] = [0x36, 0x46];
 
 #[derive(Debug, Copy, Clone, defmt::Format)]
 pub enum DecodeError {
@@ -20,10 +23,24 @@ pub enum DecodeError {
     DataReadyCrcMismatch,
 }
 
-#[derive(Debug, defmt::Format)]
-pub enum Error {
-    I2c,
+#[derive(Debug)]
+pub enum Error<E> {
+    I2c(E),
     Decode(DecodeError),
+}
+
+impl<E> defmt::Format for Error<E> {
+    fn format(&self, f: defmt::Formatter) {
+        match self {
+            Error::I2c(_) => {
+                defmt::write!(f, "I2C error");
+            }
+
+            Error::Decode(e) => {
+                defmt::write!(f, "Decode error: {:?}", e);
+            }
+        }
+    }
 }
 
 pub struct Scd40<I2C> {
@@ -38,48 +55,58 @@ where
         Self { i2c }
     }
 
-    pub async fn start(&mut self) -> Result<(), Error> {
-        self.i2c
-            .write(SCD40_ADDR, &CMD_START_PERIODIC_MEASUREMENT)
-            .await
-            .map_err(|_| Error::I2c)?;
+    pub async fn start(&mut self) -> Result<(), Error<I2C::Error>> {
+        // Stop previous measurement if it was running
+        self.send_command(&CMD_STOP_PERIODIC_MEASUREMENT).await?;
+
+        // This command needs ~500ms execution time
+        Timer::after(Duration::from_millis(500)).await;
+
+        // Reset internal state
+        self.send_command(&CMD_REINIT).await?;
+
+        Timer::after(Duration::from_millis(30)).await;
+
+        // Start measurements
+        self.send_command(&CMD_START_PERIODIC_MEASUREMENT).await?;
 
         Ok(())
     }
 
-    pub async fn data_ready(&mut self) -> Result<bool, Error> {
+    pub async fn data_ready(&mut self) -> Result<bool, Error<I2C::Error>> {
         let mut buffer = [0u8; 3];
 
-        self.i2c
-            .write(SCD40_ADDR, &CMD_GET_DATA_READY_STATUS)
-            .await
-            .map_err(|_| Error::I2c)?;
+        self.send_command(&CMD_GET_DATA_READY_STATUS).await?;
 
         self.i2c
             .read(SCD40_ADDR, &mut buffer)
             .await
-            .map_err(|_| Error::I2c)?;
+            .map_err(Error::I2c)?;
 
         // Decode status word here
         Ok(is_data_ready(&buffer).map_err(|_| Error::Decode(DecodeError::DataReadyCrcMismatch))?)
     }
 
-    pub async fn read(&mut self) -> Result<CO2SensorData, Error> {
+    pub async fn read(&mut self) -> Result<CO2SensorData, Error<I2C::Error>> {
         let mut buffer = [0u8; 9];
 
-        self.i2c
-            .write(SCD40_ADDR, &CMD_READ_MEASUREMENT)
-            .await
-            .map_err(|_| Error::I2c)?;
+        self.send_command(&CMD_READ_MEASUREMENT).await?;
 
         Timer::after(Duration::from_millis(1)).await;
 
         self.i2c
             .read(SCD40_ADDR, &mut buffer)
             .await
-            .map_err(|_| Error::I2c)?;
+            .map_err(Error::I2c)?;
 
         decode(&buffer).map_err(Error::Decode)
+    }
+
+    async fn send_command(&mut self, command: &[u8; 2]) -> Result<(), Error<I2C::Error>> {
+        self.i2c
+            .write(SCD40_ADDR, command)
+            .await
+            .map_err(Error::I2c)
     }
 }
 
