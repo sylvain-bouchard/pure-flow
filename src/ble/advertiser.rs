7931e56996892da813f8851328e141b24f8f6db0
Sylvain Bouchard <sylvain.bouchard@live.ca>
2026-07-25 17:13:49 -0400

    feat: integrate BLE & Trouble

--- src/ble/advertiser.rs
+++ src/ble/advertiser.rs
@@ -2,13 +2,49 @@ use crate::ble::sensor_packet::EnvironmentPacket;
 use crate::domain::sensor_data::EnvironmentData;
 use crate::transport::{TelemetryTransport, TransportError};
 
-use defmt::info;
+use embassy_executor::Spawner;
+// -----------------------------------------------------------------------------
+// BLE & Trouble
+// -----------------------------------------------------------------------------
+use nrf_sdc::SoftdeviceController;
+use static_cell::StaticCell;
+use trouble_host::prelude::*;
 
-pub struct BleAdvertiser {}
+// -----------------------------------------------------------------------------
+// Logging / panic
+// -----------------------------------------------------------------------------
+use defmt::{info, unwrap};
+
+const CONNECTIONS_MAX: usize = 1;
+const L2CAP_CHANNELS_MAX: usize = 1;
+
+static RESOURCES: StaticCell<
+    HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX>,
+> = StaticCell::new();
+static STACK: StaticCell<Stack<'static, SoftdeviceController<'static>, DefaultPacketPool>> =
+    StaticCell::new();
+
+pub struct BleAdvertiser {
+    peripheral: Peripheral<'static, SoftdeviceController<'static>, DefaultPacketPool>,
+}
 
 impl BleAdvertiser {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(controller: SoftdeviceController<'static>, spawner: Spawner) -> Self {
+        let resources = RESOURCES.init(HostResources::new());
+        let address = Address::random([0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa]);
+
+        let stack = trouble_host::new(controller, resources)
+            .set_random_address(address)
+            .build();
+
+        let stack: &'static mut Stack<'static, SoftdeviceController<'static>, DefaultPacketPool> =
+            STACK.init(stack);
+
+        spawner.spawn(unwrap!(trouble_background_task(stack.runner())));
+
+        Self {
+            peripheral: stack.peripheral(),
+        }
     }
 
     async fn send_adv(&mut self, payload: &[u8]) -> Result<(), TransportError> {
@@ -43,5 +79,14 @@ impl TelemetryTransport for BleAdvertiser {
         let payload = packet.encode();
 
         self.send_adv(&payload).await
+    }
+}
+
+#[embassy_executor::task]
+async fn trouble_background_task(
+    mut runner: Runner<'static, SoftdeviceController<'static>, DefaultPacketPool>,
+) {
+    loop {
+        let _ = runner.run().await;
     }
 }
